9124481d5e6202b42311203fc956ae571bc5e109
Yuki <workcomputer.com>
2023-05-25 08:17:24 +0800

    更新路径

--- main.cpp
+++ main.cpp
@@ -96,11 +96,11 @@ void multi(string& model_path, string& meta_path, string& image_dir, string& sav
 
 int main() {
     // 注意使用非patchcore模型时报错可以查看utils.cpp中infer_height和infer_width中的[1] 都改为 [0]，具体查看注释和metadata.json文件
-    string model_path = "D:/code/anomalib/results/patchcore/mvtec/bottle/run/weights/openvino/model.onnx";
-    string param_path = "D:/code/anomalib/results/patchcore/mvtec/bottle/run/weights/openvino/metadata.json";
-    string image_path = "D:/code/anomalib/datasets/MVTec/bottle/test/broken_large/000.png";
-    string image_dir  = "D:/code/anomalib/datasets/MVTec/bottle/test/broken_large";
-    string save_dir   = "D:/code/anomalib-onnxruntime-cpp/result"; // 注意目录不会自动创建,要手动创建才会保存
+    string model_path = "D:/ml/code/anomalib/results/patchcore/mvtec/bottle/run/weights/openvino/model.onnx";
+    string param_path = "D:/ml/code/anomalib/results/patchcore/mvtec/bottle/run/weights/openvino/metadata.json";
+    string image_path = "D:/ml/code/anomalib/datasets/MVTec/bottle/test/broken_large/000.png";
+    string image_dir  = "D:/ml/code/anomalib/datasets/MVTec/bottle/test/broken_large";
+    string save_dir   = "D:/ml/code/anomalib-onnxruntime-cpp/result"; // 注意目录不会自动创建,要手动创建才会保存
     string device     = "cuda";
 
     single(model_path, param_path, image_path, save_dir, device);
